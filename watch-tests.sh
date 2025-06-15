36ad1bc8155ba1668a5760910adcf6775a5970c7
treadwell <brooks.ken@gmail.com>
2025-06-15 18:26:36 -0400

    WIP: spectral analysis

--- watch-tests.sh
+++ watch-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o pipefail
 set -e
+Rscript -e "devtools::test()"
 fswatch -vr -e tests/testthat/_snaps R/ tests/ | while read f; do 
     echo "\nFile changed: $f\n"
     sleep 0.1
